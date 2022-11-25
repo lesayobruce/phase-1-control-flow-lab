4af44df81a7299ded35dff0328e088afe816fa4c
mashughuli <lesayobruce419@gmail.com>
2022-11-25 13:54:57 -0500

    completed the assignment

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  if (ride<=400){
+    return "This one is on me!";
+  }else if (ride>2000 && ride <=2500){
+    return "I Will gladly take your thirty bucks.";
+  }else if (ride >2500) {
+    return "No Can do";
+  }else{
+    return "No ride available";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city==="NYC"? "oK, Sounds good." :"No go.";
+
+}
+console.log(ternaryCheckCity("NYC"));
+
+function switchOnCharmFromTip(tip){
+  if (tip==="generous"){
+    return "Thank you."
+  }else{
+    return"Thank You."
+  } else {
+ return "Bye."
+  }
+
 }
